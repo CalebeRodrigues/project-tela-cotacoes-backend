10573776dd2bbe1ba125d97909f0558949974bb8
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-05-27 16:26:37 -0300

    Stock - Validator

--- backend/src/main/java/com/project/telaCotacoes/controller/StockController.java
+++ backend/src/main/java/com/project/telaCotacoes/controller/StockController.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +26,7 @@ public class StockController {
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE
 	)
-	public ResponseEntity<StockDTO> save (@RequestBody StockDTO dto) {
+	public ResponseEntity<StockDTO> save (@Valid @RequestBody StockDTO dto) {
 		return ResponseEntity.ok(dto);
 	}
 	
@@ -32,7 +34,7 @@ public class StockController {
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE
 	)
-	public ResponseEntity<StockDTO> update (@RequestBody StockDTO dto) {
+	public ResponseEntity<StockDTO> update (@Valid @RequestBody StockDTO dto) {
 		return ResponseEntity.ok(dto);
 	}
 	
--- backend/src/main/java/com/project/telaCotacoes/model/dto/StockDTO.java
+++ backend/src/main/java/com/project/telaCotacoes/model/dto/StockDTO.java
@@ -2,13 +2,33 @@ package com.project.telaCotacoes.model.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StockDTO {
 	
 	private Long id;
+	
+	@NotNull
 	private String name;
+	
+	@NotNull
+	@DecimalMin(value = "0.00")
+	@Digits(integer = 6, fraction = 2)
 	private Double price;
+	
+	@NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
 	private LocalDate date;
+	
+	@NotNull
+	@Digits(integer = 3, fraction = 2)
 	private Double variation;
+	
+	
 	public Long getId() {
 		return id;
 	}
