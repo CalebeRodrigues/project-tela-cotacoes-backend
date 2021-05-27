8fa2a740cfb105d24696d52736fd9ee136f7db29
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-05-27 12:05:13 -0300

    First commit

---
+++ backend/src/main/java/com/project/telaCotacoes/TelaCotacoesApplication.java
@@ -0,0 +1,13 @@
+package com.project.telaCotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelaCotacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelaCotacoesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/telaCotacoes/TelaCotacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.telaCotacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelaCotacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
