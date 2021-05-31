1e9f5a4272cba9e13cc6db3e7996ba8a4b8d3a39
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-05-31 14:54:24 -0300

    Criando camada service e exceptions handlers

--- backend/src/main/java/com/project/telaCotacoes/service/StockService.java
+++ backend/src/main/java/com/project/telaCotacoes/service/StockService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.project.telaCotacoes.exceptions.BusinessException;
 import com.project.telaCotacoes.mapper.StockMapper;
 import com.project.telaCotacoes.model.Stock;
 import com.project.telaCotacoes.model.dto.StockDTO;
 import com.project.telaCotacoes.repository.StockRepository;
+import com.project.telaCotacoes.util.MessageUtils;
 
 @Service
 public class StockService {
@@ -23,6 +25,12 @@ public class StockService {
 	@Transactional
 	public StockDTO save(StockDTO dto) {
 		Optional<Stock> optional = repository.findByNameAndDate(dto.getName(), dto.getDate());
+		
+		System.out.println("Entrei no service!");
+		
+		if (optional.isPresent()) {
+			throw new BusinessException(MessageUtils.STOCK_ALREADY_EXIST);
+		}
 		
 		Stock stock = mapper.toEntity(dto);
 		repository.save(stock);
---
+++ backend/src/main/java/com/project/telaCotacoes/util/MessageUtils.java
@@ -0,0 +1,8 @@
+package com.project.telaCotacoes.util;
+
+public abstract class MessageUtils {
+
+	public static final String
+		STOCK_ALREADY_EXIST = "Stock already exists in the database";
+	
+}
